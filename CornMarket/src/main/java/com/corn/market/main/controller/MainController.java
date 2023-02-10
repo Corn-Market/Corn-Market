c426637270082b30f31dbcdfbb0b88768624c0fd
CH <hbac11@naver.com>
2023-02-10 22:13:51 +0900

    메인 페이지 매핑

--- CornMarket/src/main/java/com/corn/market/main/controller/MainController.java
+++ CornMarket/src/main/java/com/corn/market/main/controller/MainController.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	
-	@GetMapping("/main")
+	@GetMapping("/")
 	public String mainPage() {
+		return "/main/index";
+	}
+	@GetMapping("/main")
+	public String mainPage2() {
 		return "/main/index";
 	}
 	
