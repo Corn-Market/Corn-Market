f017548405c3f333c26ff09ac6b6551810b11ae3
ch <hbac11@naver.com>
2023-02-24 16:22:40 +0900

    [refactor] town 도메인 mybatis로 리팩토링 #56

--- CornMarket/src/main/java/com/corn/market/town/service/TownService.java
+++ CornMarket/src/main/java/com/corn/market/town/service/TownService.java
@@ -1,15 +1,7 @@
 package com.corn.market.town.service;
 
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.corn.market.town.dao.TownDao;
 import com.corn.market.town.domain.TownCertification;
@@ -18,37 +10,31 @@ import com.corn.market.town.domain.TownInfo;
 @Service
 public class TownService {
 	
-	@Autowired
-	private TownDao townDao;
-	@Autowired
-	private DataSource dataSource;
-	
+	private final TownDao townDao;
+
+	public TownService(TownDao townDao) {
+		this.townDao = townDao;
+	}
+
 	//town code 가져오기
-	public String getTownCode(TownInfo townInfo) {
+	public String getTownCode(TownInfo townInfo) throws Exception {
+		townInfo.setTown_name(townInfo.getTown_name().split(" ")[1]);
 		String town_code = townDao.selectTownCode(townInfo);
 		return town_code;
 	}
 	
 	//동네인증 이전기록 삭제하고 새로운 인증 등록
-	public void deleteAndInputAddress(TownCertification town) {
-		//트랜잭션
-		PlatformTransactionManager tm = new DataSourceTransactionManager(dataSource);
-		TransactionStatus status = tm.getTransaction(new DefaultTransactionDefinition());
+	@Transactional(rollbackFor = Exception.class)
+	public void deleteAndInputAddress(TownCertification town) throws Exception {
 		//이전 인증기록 개수
 		int count = townDao.selectCountAddress(town.getUser_id());
-		try {
-			if(count==0) {
-				townDao.insertNewAddress(town);
-				tm.commit(status);
-			}else {
-				townDao.deletePrevAddress(town.getUser_id());
-				townDao.insertNewAddress(town);
-				tm.commit(status);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			tm.rollback(status);
+		if(count==0) {
+			townDao.insertNewAddress(town);
+		}else {
+			townDao.deletePrevAddress(town.getUser_id());
+			townDao.insertNewAddress(town);
 		}
+
 	}
 
 }
