82fa1d3eb7cdcb354ddeead376d6489d69937036
CH <hbac11@naver.com>
2023-12-03 14:58:13 +0900

    websocket wss로 변경

--- CornMarket/src/main/webapp/resources/js/chatting/chatting_pop.js
+++ CornMarket/src/main/webapp/resources/js/chatting/chatting_pop.js
@@ -1,7 +1,7 @@
 // 웹소켓 연결
 var webSocket;
 function connect() {
-  webSocket = new WebSocket('ws://43.202.105.22:443/chat');
+  webSocket = new WebSocket('wss://43.202.105.22:443/chat');
   //webSocket = new WebSocket('ws://localhost:8090/chat');
 
   webSocket.onopen = onOpen;
